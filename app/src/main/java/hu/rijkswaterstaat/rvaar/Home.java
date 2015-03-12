0e9b85d50bb23af5677c27018f7bf6cbcae8ce09
haydar100 <haydar.yilmaz@student.hu.nl>
2015-03-12 14:47:35 +0100

    Removed DebugPackage

--- app/src/main/java/hu/rijkswaterstaat/rvaar/Home.java
+++ app/src/main/java/hu/rijkswaterstaat/rvaar/Home.java
@@ -1,13 +1,12 @@
 package hu.rijkswaterstaat.rvaar;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 
