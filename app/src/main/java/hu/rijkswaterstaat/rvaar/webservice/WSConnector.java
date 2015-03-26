f4301ebe13bd5618101742ba8fbbce8fd16c683a
Haydar <haydar.yilmaz@gmail.com>
2015-03-26 12:55:35 +0100

    Able to communicate with webservice, only from eduroam !

--- app/src/main/java/hu/rijkswaterstaat/rvaar/webservice/WSConnector.java
+++ app/src/main/java/hu/rijkswaterstaat/rvaar/webservice/WSConnector.java
@@ -1,20 +1,63 @@
 package hu.rijkswaterstaat.rvaar.webservice;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.SoapFault;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpResponseException;
+import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 /**
  * Created by Berkan on 25-3-2015.
  */
-public class WSConnector extends AsyncTask {
+public class WSConnector extends AsyncTask<String, Void, ArrayList<MarkerOptions>> {
     final static String NAMESPACE = "http://tempuri.org/";
-    final static String METHOD_NAME = "geefKruispunten";
-    final static String SOAP_ACTION = "http://tempuri.org/GetContent";
-    final static String URL = "http://www.example.com/XMLGenerator/GenerateXML.asmx";
+    final static String URL = "http://145.89.159.136:8733/Design_Time_Addresses/RVaarWebService//RVaarServiceImpl/?wsdl";
+
+
+    public ArrayList<MarkerOptions> getMarkers() {
+        SoapObject request = new SoapObject(NAMESPACE, "geefKruispunten");
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+        envelope.setOutputSoapObject(request);
+        envelope.dotNet = true;
+
+        try {
+            HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+            androidHttpTransport.call("http://tempuri.org/RVaarService/geefKruispunten", envelope);
+            SoapObject result = (SoapObject) envelope.getResponse();
+            final int PropertyCount = result.getPropertyCount();
+            Log.d("Propertycount", "" + result.getPropertyCount());
+            if (result != null) {
+
+            }
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (SoapFault soapFault) {
+            soapFault.printStackTrace();
+        } catch (HttpResponseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
 
 
     @Override
-    protected Object doInBackground(Object[] params) {
-        return null;
+    protected ArrayList<MarkerOptions> doInBackground(String... params) {
+        Log.d("doInBackground", "doInBackground !");
+        return getMarkers();
     }
 }
