0dffc83bc9f741a0b2ba2272f40fb67948b4b2d0
ingo <ingo.vanleeuwen@student.hu.nl>
2015-03-26 12:02:16 +0100

    home afgemaakt  + drawables erbij , Hardcoded Niveau scherm

---
+++ app/src/main/java/hu/rijkswaterstaat/rvaar/Niveau.java
@@ -0,0 +1,62 @@
+package hu.rijkswaterstaat.rvaar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by ingo on 26-3-2015.
+ */
+public class Niveau extends ActionBarActivity  {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_LEFT_ICON);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, R.drawable.ic_rvaar);
+
+
+
+    }
+
+
+    public void buttonOnClick(View v){
+
+        ImageButton button =(ImageButton) v;
+        startActivity(new Intent(getApplicationContext(), OverviewMap.class));
+/**/
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
