994acb1f55a692c244fb6bb5aab577815de32ec8
Haydar Yilmaz <Haydar.yilmaz@gmail.com>
2015-04-08 16:47:19 +0200

    Fixed layout (Settings)

--- app/src/main/java/hu/rijkswaterstaat/rvaar/utils/PreferencesActivity.java
+++ app/src/main/java/hu/rijkswaterstaat/rvaar/utils/PreferencesActivity.java
@@ -1,18 +1,24 @@
 package hu.rijkswaterstaat.rvaar.utils;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
 import hu.rijkswaterstaat.rvaar.R;
+import hu.rijkswaterstaat.rvaar.menu.MenuActivity;
 
 /**
  * Created by Haydar on 07-04-15.
  */
-public class PreferencesActivity extends PreferenceActivity {
+public class PreferencesActivity extends MenuActivity {
+    static String[] drawerItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_settings);
+        drawerItems = getResources().getStringArray(R.array.drawerItems);
+        setMenu(drawerItems);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferenceFragment()).commit();
+
+
     }
 
     public static class PreferenceFragment extends android.preference.PreferenceFragment {
