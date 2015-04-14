3820a23ec75ecccc62b18f102dd4693b44fea837
MayinBun <bunyamin.gs@gmail.com>
2015-04-14 23:30:51 +0200

    -v7.support Toolbar/ActionbarToggle -bunch of changes to styles.xml -changes to layouts -SetMenu() toggle listener

--- app/src/main/java/hu/rijkswaterstaat/rvaar/menu/MenuActivity.java
+++ app/src/main/java/hu/rijkswaterstaat/rvaar/menu/MenuActivity.java
@@ -1,44 +1,61 @@
 package hu.rijkswaterstaat.rvaar.menu;
 
-import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import hu.rijkswaterstaat.rvaar.Home;
 import hu.rijkswaterstaat.rvaar.R;
-import hu.rijkswaterstaat.rvaar.TipsContentActivity;
 
 
 public class MenuActivity extends ActionBarActivity {
     private String mDrawerItems[];
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
     // used to store app title
+    private CharSequence mDrawerTitle;
     private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerItems = getResources().getStringArray(R.array.drawerItems);
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDrawerItems));
-    }
 
+    }
     public void setMenu(String[] items) {
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerItems = getResources().getStringArray(R.array.drawerItems);
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
-    }
+        Toolbar mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+
+        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,R.string.drawer_open,R.string.drawer_closed){
+            public void onDrawerClosed(View view){
+                super.onDrawerClosed(view);
+            }
+            public void onDrawerOpened(View view){
+                super.onDrawerOpened(view);
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerToggle.syncState();
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
