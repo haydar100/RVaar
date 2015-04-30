6769b7af9cb9060e83f65a9a9d2234bbae09b74f
MayinBun <bunyamin.gs@gmail.com>
2015-04-30 15:28:30 +0200

    -domein

---
+++ app/src/main/java/hu/rijkswaterstaat/rvaar/domain/TipsAndTricks.java
@@ -0,0 +1,59 @@
+package hu.rijkswaterstaat.rvaar.domain;
+
+/**
+ * Created by Haydar on 30-04-15.
+ */
+public class TipsAndTricks {
+    protected String headerName;
+    protected String content;
+    private int id;
+
+    public TipsAndTricks() {
+    }
+
+    ////dssd
+    public TipsAndTricks(String headerName, String content) {
+        super();
+        this.headerName = headerName;
+        this.content = content;
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getHeaderName() {
+
+        return headerName;
+    }
+
+    public void setHeaderName(String headerName) {
+        this.headerName = headerName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "TipsAndTricks{" +
+                "id=" + id +
+                ", headerName='" + headerName + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+}
